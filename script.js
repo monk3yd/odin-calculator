1b2592f3db17aa269763587deabcfbf87cb8cf3e
monk3yd <jegajardog@gmail.com>
2022-09-14 12:59:40 -0300

    Finish add, substract, multiply and divide funcs

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(...args) {
+    let total = args.reduce((a, b) => a + b);
+    return total;
+};
+
+function substract(...args) {
+    let total = args.reduce((a, b) => a - b);
+    return total;
+};
+
+function multiply(...args) {
+    let total = args.reduce((a,b) => a * b);
+    return total;
+};
+
+function divide(...args) {
+    let total = args.reduce((a, b) => a / b);
+    return total;
+};
