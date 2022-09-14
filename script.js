6bd88cc2a667838ddabcbaad5e01f0738b10cbc5
monk3yd <jegajardog@gmail.com>
2022-09-14 18:04:37 -0300

    Display logic when entering values seems correct

--- script.js
+++ script.js
@@ -35,13 +35,24 @@ numbers.textContent = "0";
 const buttons = document.querySelectorAll(".btn")
 buttons.forEach(button => {
     button.addEventListener("click", (e) => {
+        // pressed btn value
         let btnValue = e.target.textContent;
+        console.log(btnValue);
+
+        // Clear btn reset display to 0
         if (btnValue === "AC") {
             numbers.textContent = "0";
             return;
         }
 
-        if (numbers.textContent.length === 1 && numbers.textContent === "0") {
+        // Can't add multiple 0 as init value on display
+        if (btnValue === "0" && numbers.textContent.length === 1 && numbers.textContent === "0") {
+            return;
+        // Can't add more than one "." in display
+        } else if (btnValue === "." && numbers.textContent.includes(".")) {
+            return;
+        // Replace init 0 in display for first entered num
+        } else if (btnValue !== "0" && numbers.textContent === "0"){
             numbers.textContent = "";
         }
         numbers.textContent += btnValue;
