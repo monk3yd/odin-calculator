b2f34fa766b12790e04dca094f742c18e1885bc4
monk3yd <jegajardog@gmail.com>
2022-09-20 10:56:56 -0300

    Move functions to bottom, and refactor a little

--- script.js
+++ script.js
@@ -1,35 +1,3 @@
-function add(...args) {
-    let total = args.reduce((a, b) => a + b);
-    return total;
-};
-
-function substract(...args) {
-    let total = args.reduce((a, b) => a - b);
-    return total;
-};
-
-function multiply(...args) {
-    let total = args.reduce((a,b) => a * b);
-    return total;
-};
-
-function divide(...args) {
-    let total = args.reduce((a, b) => a / b);
-    return total;
-};
-
-function operate(operator, a, b) {
-    if (operator === "+") {
-        return add(a, b);
-    } else if (operator === "-") {
-        return substract(a, b);
-    } else if (operator === "*") {
-        return multiply(a, b);
-    } else if (operator === "/") {
-        return divide(a, b);
-    }
-};
-
 const display = document.querySelector("#display");
 display.textContent = "0";
 
@@ -43,9 +11,11 @@ buttons.forEach(button => {
         if (btnValue === "=") {
             // Inputted operation
             const chars = display.textContent
-            console.log(chars);
+            console.log(`Display: ${chars}`);
 
             const allOperators = ["*", "/", "+", "-"];
+            // Other way: iterate over chars looking for selector (read left-to-right)
+
             for (let operator of allOperators ) {
                 // Check if operator exists in given string operation
                 operatorExists = chars.indexOf(operator)
@@ -53,28 +23,23 @@ buttons.forEach(button => {
                 if (operatorExists === -1) {
                     continue;
                 }
-                // Get operator index - 1 and index + 1 for extracting operands
-                let leftOperandIndex = operatorExists - 1;
-                let rightOperandIndex = operatorExists + 1;
-
-                let operandLeft = chars[leftOperandIndex];
-                let operandRight = chars[rightOperandIndex];
+                // split string at operator for getting operands in array
+                let operands = chars.split(operator)
+                console.log(operands);
 
                 // Calculate operation result
                 let result = operate(
                     operator,
-                    Number(operandLeft),
-                    Number(operandRight)
+                    Number(operands[0]),
+                    Number(operands[1])
                 )
-                console.log(`Result: ${result}`)
-            }
 
-            // console.log(result);
-            //
-            // // Show result
-            // display.textContent = result;
-            // return;
-            // };
+                // Show result
+                console.log(`Result: ${result}`)
+                display.textContent = result;
+                return;
+
+            }
         }
 
         // Clear btn reset display to 0
@@ -97,4 +62,34 @@ buttons.forEach(button => {
     });
 })
 
+function operate(operator, a, b) {
+    if (operator === "+") {
+        return add(a, b);
+    } else if (operator === "-") {
+        return substract(a, b);
+    } else if (operator === "*") {
+        return multiply(a, b);
+    } else if (operator === "/") {
+        return divide(a, b);
+    }
+};
 
+function add(...args) {
+    let total = args.reduce((a, b) => a + b);
+    return total;
+};
+
+function substract(...args) {
+    let total = args.reduce((a, b) => a - b);
+    return total;
+};
+
+function multiply(...args) {
+    let total = args.reduce((a,b) => a * b);
+    return total;
+};
+
+function divide(...args) {
+    let total = args.reduce((a, b) => a / b);
+    return total;
+};
